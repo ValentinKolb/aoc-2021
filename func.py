713907d4d2ffa61b24678514a9db75e96c51e7c5
Valentin Kolb <valentinkolb@Valentins-Laptop.local>
2021-12-10 18:27:11 +0100

    solved day 10

--- func.py
+++ func.py
@@ -22,6 +22,10 @@ snd = lambda t: t[1]
 lst: Callable[[Iterable[T]], T]
 lst = lambda t: t[-1]
 
+# str helpers
+strip: Callable[[str], str]
+strip = lambda s: s.strip()
+
 # list helpers
 flatten: Callable[[list[list[T]], ], list[T]]
 flatten = lambda list_: [item for sublist in list_ for item in sublist]
@@ -49,5 +53,25 @@ prange = pipe(range)
 pflatten: Callable[[Iterable[Iterable[T]]], Iterable[T]]
 pflatten = pipe(flatten)
 
+
+class infix:
+
+    def __init__(self, func):
+        self.func = func
+
+    def __mod__(self, other):
+        print(f'{other=}')
+        return self.func(other)
+
+    def __call__(self, *args, **kwargs):
+        return self.func(*args, **kwargs)
+
+
 if __name__ == '__main__':
-    ...
+    print = infix(print)
+
+    print % (infix(lambda x: x * 2) % 10)
+
+    print % "test", "Test1"
+
+    print("Test", end="/n...")
